c4b9e7e48503b6c77c005b77a9ab6e7602a6da6d
Hitesh Nayak <hiteshnayak305@gmail.com>
2017-03-24 23:54:41 +0530

    reserve occupied blocks

--- memory_allocation.cpp
+++ memory_allocation.cpp
@@ -1,5 +1,5 @@
-#include<bits/stdc++.h>
 #include<iostream>
+#include<algorithm>
 using namespace std;
 class memory;
 class process{
@@ -13,8 +13,8 @@ class process{
 			this->process_no = process_no;
 			this->size = size;
 			best = NULL;
-			best = NULL;
-			best = NULL;
+			worst = NULL;
+			first = NULL;
 		}
 };
 class memory{
@@ -26,42 +26,72 @@ class memory{
 	memory(int block_no, int size){
 		this->block_no = block_no;
 		this->size = size;
-		occupied = false;
+		this->occupied = false;
+		this->process_no = -1;
 	}
 	void allocate(process p){
-		occupied = true;
+		this->occupied = true;
 		this->process_no = p.process_no; 
 	}
-	void free(process p){
-		occupied = false;
+	void free(){
+		this->occupied = false;
+		this->process_no = -1;
 	}
 };
 
+int cond(memory* m){
+	memory* mt = m;
+	if(mt == NULL)
+		return -1;
+	else
+		return mt->block_no;
+}
+
+void sort1(memory* m[],int n){
+	for(int i =0;i<n-1;i++){
+		for(int j=i+1;j<n;j++){
+			if(m[i]->size > m[j]->size)
+			{
+				memory* temp = m[i];
+				m[i] = m[j];
+				m[j] = temp;
+			}
+		}
+	}
+}
+
+
+void reset(memory* m[],int n){
+	for(int i =0;i<n;i++){
+		m[i]->free();
+	}
+}
 void first(memory* m[],process* p,int size){
 	for(int i=0;i<size;i++){
-		if(m[i]->size >= p->size){
+		if(m[i]->size >= p->size && m[i]->occupied == false){
 			p->first = m[i];
+			m[i]->allocate(*p);
 			return;
 		}
 	}
 }
 
 void worst(memory* m[],process* p,int size){
-	memory* mx = m[0];
-	for(int i=0;i<size;i++){
-		if(m[i]->size > mx->size){
-			mx = m[i];
+	for(int i=size-1;i>=0;i--){
+		if(m[i]->size >= p->size && m[i]->occupied == false){
+			p->worst = m[i];
+			m[i]->allocate(*p);
+			return;
 		}
 	}
-	p->worst = mx;
 }
 
 void best(memory* m[],process* p,int size){
-	int d = 1000000;
 	for(int i=0;i<size;i++){
-		if(m[i]->size >= p->size && m[i]->size - p->size <d){
+		if(m[i]->size >= p->size && m[i]->occupied == false){
 			p->best = m[i];
-			d = m[i]->size - p->size;
+			m[i]->allocate(*p);
+			return;
 		}
 	}
 }
@@ -87,14 +117,25 @@ int main(){
 		cout<<"enter process no and size: ";
 		cin>>a>>b;
 		p[i] = new process(a,b);
+	}
+	for(int i =0;i<l;i++){
 		first(m,p[i],n);
+	}
+	reset(m,n);
+	//sort memory block
+		sort1(m,n);
+	for(int i =0;i<l;i++){			
 		best(m,p[i],n);
+	}
+	reset(m,n);
+	for(int i =0;i<l;i++){
 		worst(m,p[i],n);
 	}
+	reset(m,n);
 	cout<<endl<<endl;
 	cout<<"\t* process\tfirst\tbest\tworst *"<<endl<<endl;
 	for(int i=0;i<l;i++){
-		cout<<"\t* "<<p[i]->process_no<<"\t\t"<<p[i]->first->block_no<<"\t"<<p[i]->best->block_no<<"\t"<<p[i]->worst->block_no<<" *"<<endl<<endl;
+		cout<<"\t* "<<p[i]->process_no<<"\t\t"<<cond(p[i]->first)<<"\t"<<cond(p[i]->best)<<"\t"<<cond(p[i]->worst)<<" \t*"<<endl<<endl;
 	}	
 	return 0;
 }
